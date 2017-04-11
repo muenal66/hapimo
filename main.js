37289a73e575e3897984ab1a36a64be167578c9d
muenal66 <hasan.cupra@yandex.com>
2017-04-11 18:24:10 +0200

    static icerik

--- main.js
+++ main.js
@@ -6,13 +6,21 @@ const server = new Hapi.Server();
 var portt = ~~process.env.PORT || 3000;
 server.connection({ port: portt, host: '0.0.0.0' });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
     }
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html');
+        }
+    });
 });
+
 
 server.start((err) => {
 
